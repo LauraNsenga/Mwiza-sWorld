c60b11719c338a8298c3fe3f4be5750308fb9ba4
LauraNsenga <97988193+LauraNsenga@users.noreply.github.com>
2025-06-04 17:36:53 -0500

    Delete src directory

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-export const getImageUrl = (path) => {
-    return new URL(`/assets/${path}`, import.meta.url).href;
-    // gets relative path of image 
-    // and 
-    // import.meta.url: base URL of my website 
-    // and appends the relative path to the website URL  
-    // make it easy to dynamically import images. 
-}
